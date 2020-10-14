825ecf27bdec3b894c365ba824fe4cfd99ccf652
Vintall <eg.ilyashenko@gmail.com>
2020-10-14 17:51:07 +0300

    Add two models (Account and Profile), update xamarin framework to last version

---
+++ ProfileBook/ProfileBook/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ProfileBook.Models
+{
+    [Table("Accounts")]
+    class Account
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        [Column("_login")]
+        public string login { get; set; }
+        [Column("_password")]
+        public string password { get;set; }
+        
+    }
+}
---
+++ ProfileBook/ProfileBook/Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ProfileBook.Models
+{
+    [Table("Profiles")]
+    class Profile
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int id { get; set; }
+        [Column("_first_name")]
+        public string first_name { get; set; }
+        [Column("_last_name")]
+        public string last_name { get; set; }
+        [Column("_owner_id")]
+        public int owner_id { get; set; }
+    }
+}
